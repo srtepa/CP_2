50eb4656f8d00fd5cb56010db4eccd7bec2c3b57
srtepa <stepankonon1@gmail.com>
2025-10-19 20:03:07 +0300

    ф-ция хеширования пароля

--- course_project/course_project/Services/AuthService.cs
+++ course_project/course_project/Services/AuthService.cs
@@ -1,12 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using BCrypt.Net;
+using System;
 
 namespace course_project.Services
 {
     internal class AuthService
     {
+        public static string HashPassword(string password)
+        {
+            //ошибка: если пуста строка
+            if (string.IsNullOrEmpty(password))
+            {
+                throw new ArgumentException("Пароль не может быть пустым.", nameof(password));
+            }
+
+            return BCrypt.Net.BCrypt.HashPassword(password, workFactor: 10);
+        }
+
+        //функция проверки пароля
+        public static bool VerifyPassword(string password, string hashedPassword)
+        {
+            return BCrypt.Net.BCrypt.Verify(password, hashedPassword);
+        }
     }
 }
