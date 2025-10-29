894131486ba0a2d06bdfdc13e19ddc6e5608e927
srtepa <stepankonon1@gmail.com>
2025-10-29 16:59:20 +0300

    создал класс Report и вспомогательный класс BestSellingProductInfo

---
+++ course_project/course_project/Models/BestSellingProductInfo.cs
@@ -0,0 +1,9 @@
+namespace course_project.Models;
+
+internal class BestSellingProductInfo
+{
+    public string ProductName { get; set; }
+    public string ProductArticle { get; set; }
+    public int TotalQuantitySold { get; set; }
+    public decimal TotalRevenueFromProduct { get; set; }
+}
---
+++ course_project/course_project/Models/Report.cs
@@ -0,0 +1,26 @@
+namespace course_project.Models;
+
+internal class Report
+{
+    public string Title { get; set; } //название отчета, например, "Отчет по продажам"
+    public DateTime CreationDate { get; set; }
+    public string CreatedByUser { get; set; }
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
+    
+    //итоговые показатели
+    public decimal TotalRevenue { get; set; }
+    public int TotalSalesCount { get; set; }
+    public int TotalItemsSold { get; set; }
+    public decimal AverageCheckValue { get; set; }
+    
+    public List<Sale> Sales { get; set; } //список всех продаж, попавших в отчет
+    public List<BestSellingProductInfo> TopSellingProducts { get; set; }
+    
+    public Report()
+    {
+        // Инициализируем списки, чтобы они не были null
+        Sales = new List<Sale>();
+        TopSellingProducts = new List<BestSellingProductInfo>();
+    }
+}
