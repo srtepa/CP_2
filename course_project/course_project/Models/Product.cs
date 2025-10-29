6d9480738d7bb576ab09221cf1a0bad5a942fca6
srtepa <stepankonon1@gmail.com>
2025-10-29 16:44:42 +0300

    добавил класс Products.cs и Products.json

---
+++ course_project/course_project/Models/Product.cs
@@ -0,0 +1,12 @@
+namespace course_project.Models;
+
+internal class Product
+{
+    public int Id { get; set; }
+    public string Article { get; set; }
+    public string Name { get; set; }
+    public string Category { get; set; }
+    public double Price { get; set; }
+    public string QuantityInStok { get; set; }
+    
+}
