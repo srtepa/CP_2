2af5c83ec1ecf1aed70becc902d95442b9cdb596
srtepa <stepankonon1@gmail.com>
2025-10-29 16:50:54 +0300

    добавил класс Sale и SaleItem

---
+++ course_project/course_project/Models/Sale.cs
@@ -0,0 +1,10 @@
+namespace course_project.Models;
+
+internal class Sale
+{
+    public int SaleId { get; set; }
+    public DateTime SaleDate { get; set; }
+    public string SellerUserName { get; set; }
+    public decimal TotalAmount { get; set; }
+    public List<SaleItem> Items { get; set; }
+}
